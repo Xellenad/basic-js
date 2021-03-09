04e696f2e16e7f6ed654342a584ee8378a10fbd5
Xellenad <Xellenad54@gmail.com>
2021-03-09 18:19:28 +0300

    complite task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,14 +4,14 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  // throw new CustomError('Not implemented');
-  if (typeof (sampleActivity) !== 'string' || +sampleActivity < 1 || +sampleActivity > 15 || !sampleActivity) {
+  
+  if (typeof (sampleActivity) !== 'string' || +sampleActivity < 1 || +sampleActivity > 15 || !sampleActivity || !isFinite(sampleActivity) ) {
     return false;
   }
 
   const i = Math.log(2) / HALF_LIFE_PERIOD;
   const j = Math.log(MODERN_ACTIVITY / sampleActivity) / i;
 
-  return j;
+  return parseFloat((j).toFixed(2));
 
 };
